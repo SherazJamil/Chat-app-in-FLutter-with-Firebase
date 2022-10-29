4a38b02bc7dd1872e4fdf63b15a6659595d14c47
SherazJamil <sherazjamil463@gmail.com>
2022-10-29 09:28:48 +0500

    Add Splash screen

---
+++ lib/Splash Screen/SplashScreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreenView(
+      navigateRoute: const Loginscreen(),
+      duration: 3000,
+      imageSize: 150,
+      imageSrc: "assets/Chat.png",
+      text: "Mi Chat",
+      textType: TextType.ScaleAnimatedText,
+      textStyle: const TextStyle(
+        color: Colors.deepOrange,
+        fontSize: 30.0,
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
