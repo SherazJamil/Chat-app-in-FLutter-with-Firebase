d8b5cda53cdd07e80255a0cabd9ce2f041ae355e
SherazJamil <sherazjamil463@gmail.com>
2022-10-29 09:54:37 +0500

    SignUp screen UI created

---
+++ lib/Login & SignUp/SignUp.dart
@@ -0,0 +1,325 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'Login.dart';
+
+class SignUpscreen extends StatefulWidget {
+  const SignUpscreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpscreen> createState() => _SignUpscreenState();
+}
+
+class _SignUpscreenState extends State<SignUpscreen> {
+
+  File? image;
+
+  Future pickImage() async {
+    // try {
+      final image = await ImagePicker().pickImage(
+        source: ImageSource.gallery,
+      );
+      if (image == null) return;
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    // } on PlatformException catch (e) {
+    //   print('Failed to pick image: $e');
+    // }
+  }
+
+  final emailEditingController = TextEditingController();
+  final passwordEditingController = TextEditingController();
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: ListView(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 190,
+                  width: 170,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.elliptical(400, 400)),
+                    gradient: LinearGradient(
+                        colors: [Color(0xffffa500), Colors.deepOrangeAccent]),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12.0),
+                child: Row(
+                  children: const [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "S",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "I",
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                    Text(
+                      "G",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                    Text(
+                      "N",
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Text(
+                      "U",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                    Text(
+                      "p",
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.orange,
+                      width: 4,
+                    ),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(500),
+                    ),
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      pickImage();
+                    },
+                    child: ClipOval(
+                      child: Container(
+                        height: 120,
+                        width: 120,
+                        child: const Icon(
+                            FontAwesomeIcons.image,
+                            color: Colors.black,
+                            size: 40),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Form(
+                key: formkey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                        controller: emailEditingController,
+                        decoration: InputDecoration(
+                            hintText: "Enter your Email",
+                            hintStyle: const TextStyle(
+                              color: Colors.black54,
+                            ),
+                            fillColor: Colors.grey.shade300,
+                            filled: true,
+                            prefixIcon: const Icon(
+                              FontAwesomeIcons.solidEnvelope,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide.none,
+                            )),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                        controller: passwordEditingController,
+                        decoration: InputDecoration(
+                            hintText: "Enter your Password",
+                            hintStyle: const TextStyle(
+                              color: Colors.black54,
+                            ),
+                            fillColor: Colors.grey.shade300,
+                            filled: true,
+                            prefixIcon: const Icon(
+                              FontAwesomeIcons.lock,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide.none,
+                            )),
+                        obscureText: true,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: InkWell(
+                        onTap: () {
+                          showDialog<void>(
+                            context: context,
+                            barrierDismissible: true,
+                            // false = user must tap button, true = tap outside dialog
+                            builder: (BuildContext dialogContext) {
+                              return AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                title: const Text(
+                                  'MiChat SignUp',
+                                  style: TextStyle(
+                                    color: Colors.deepOrange,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                content: const Text(
+                                  'You have successfully SignUp',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w300,
+                                      color: Colors.black45
+                                  ),
+                                ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text(
+                                      'Ok',
+                                      style: TextStyle(
+                                          color: Colors.deepOrange,
+                                          fontWeight: FontWeight.bold
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(context, MaterialPageRoute(builder: (context) => const Loginscreen())); // Dismiss alert dialog
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: Container(
+                          height: 45,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: const LinearGradient(colors: [
+                              Color(0xffffa500),
+                              Colors.deepOrangeAccent,
+                            ]),
+                          ),
+                          child: const Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "SignUp",
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 19),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Already have an account ?",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 4.0,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Loginscreen()));
+                            },
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.deepOrange,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
